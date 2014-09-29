b4dca76a95b5466b9f61182a7106f8761198c918
d4ryus <w.wackerbauer@yahoo.de>
2014-09-29 17:02:23 +0200

    implemented performance indicator

--- src/gui/panels/exercise/PerformancePanel.java
+++ src/gui/panels/exercise/PerformancePanel.java
@@ -34,6 +34,7 @@ public class PerformancePanel extends JPanel
 	private JLabel lblSuccess;
 	private JLabel lblLastFail;
 	private JPanel panel;
+	private PerfIndicator pi;
 
 	public PerformancePanel()
 	{
@@ -80,45 +81,6 @@ public class PerformancePanel extends JPanel
 		lblLastFail.setToolTipText("Last Reverse Fail");
 		lblLastFail.setHorizontalAlignment(SwingConstants.CENTER);
 		add(lblLastFail, "3, 2, fill, fill");
-
-		PerfIndicator pi = new PerfIndicator();
-		panel = new JPanel();
-		panel.add(pi);
-		add(panel, "1, 4, 3, 1, fill, fill");
-		panel.setLayout(new GridLayout(0, 1, 0, 0));
-	}
-
-	class PerfIndicator extends JPanel
-	{
-		private static final long serialVersionUID = -287247801336202957L;
-
-		private void doDrawing(Graphics g)
-		{
-			Graphics2D g2d = (Graphics2D) g;
-			g2d.setColor(Color.red);
-
-			for (int i = 0; i <= 1000; i++)
-			{
-				Dimension size = getSize();
-				Insets insets = getInsets();
-
-				int w = size.width  - insets.left - insets.right;
-				int h = size.height - insets.top  - insets.bottom;
-
-				Random r = new Random();
-				int x = Math.abs(r.nextInt()) % w;
-				int y = Math.abs(r.nextInt()) % h;
-				g2d.drawLine(x, y, x, y);
-				g2d.drawLine(0, h/2, w, h/2);
-			}
-		}
-
-		@Override
-		public void paintComponent(Graphics g)
-		{
-			super.paintComponent(g);
-			doDrawing(g);
-		}
 	}
 
 	public void updateInfo(Phrase phr)
@@ -128,12 +90,80 @@ public class PerformancePanel extends JPanel
 			lblSuccess.setText(new Integer(phr.perf.revSuccess).toString());
 			lblLastSuccess.setText(TimeCalc.calcPrettyTime(phr.perf.revLastSuccess));
 			lblLastFail.setText(TimeCalc.calcPrettyTime(phr.perf.revLastFail));
+			pi = new PerfIndicator(phr.perf.revSuccess, phr.perf.max);
 		}
 		else
 		{
 			lblSuccess.setText(new Integer(phr.perf.success).toString());
 			lblLastSuccess.setText(TimeCalc.calcPrettyTime(phr.perf.lastSuccess));
 			lblLastFail.setText(TimeCalc.calcPrettyTime(phr.perf.lastFail));
+			pi = new PerfIndicator(phr.perf.success, phr.perf.max);
+		}
+		panel = new JPanel();
+		panel.setLayout(new GridLayout(0, 1, 0, 0));
+		panel.add(pi);
+		add(panel, "1, 4, 3, 1, fill, fill");
+	}
+	
+	class PerfIndicator extends JPanel
+	{
+		private static final long serialVersionUID = -287247801336202957L;
+		private int success;
+		private int max;
+		
+		public PerfIndicator(int success, int max)
+		{
+			super();
+			this.success = success;
+			this.max = max;
+		}
+
+		@Override
+		public void paintComponent(Graphics g)
+		{
+			super.paintComponent(g);
+			doDrawing(g);
+		}
+		
+		private void doDrawing(Graphics g)
+		{
+			Graphics2D g2d = (Graphics2D) g;
+			g2d.setColor(Color.red);
+
+			Dimension size = getSize();
+			Insets insets = getInsets();
+
+			int w = size.width  - insets.left - insets.right;
+			int h = size.height - insets.top  - insets.bottom;
+			int middle = w/2;
+			
+			g2d.setColor(Color.BLACK);
+			g2d.drawLine(0, h/2, w, h/2);
+			g2d.drawLine(w/2, 0, w/2, h);
+			
+			int barLength = 0;
+			int barHeight = 0;
+			if (max != 0)
+			{
+				barLength = (int)((double)middle/(double)max * (double)(success < 0 ? -1 * success : success));
+				barHeight = h-11;
+			}
+				
+			if (success > 0)
+			{
+				g2d.setColor(Color.GREEN);
+				g2d.fillRect(middle+1, 5, barLength, barHeight);
+			}
+			else if (success < 0)
+			{
+				g2d.setColor(Color.RED);
+				g2d.fillRect(middle - barLength, 5, barLength, barHeight);
+			}
+			else
+			{
+				g2d.setColor(Color.BLUE);
+				g2d.fillRect(middle -5, 5, 10, barHeight);
+			}
 		}
 	}
 }
